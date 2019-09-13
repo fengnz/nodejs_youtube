405e7b9dca8f92c5011c1ddee93a537f65f495db
CATCH\dengy <yufeng.deng@catchsoftware.com>
2019-09-14 11:54:22 +1200

    Add post method

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 module.exports = router;
