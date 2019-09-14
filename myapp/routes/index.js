f3a2c7a30a27df654320b9e10cdc73a78f8802aa
CATCH\dengy <yufeng.deng@catchsoftware.com>
2019-09-14 12:10:58 +1200

    Read user input

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+  console.log(req.body);
   res.render('index', { title: 'Express' });
 });
 module.exports = router;
