45e9b021f21e26f2ba2f26c7da8ef89067df533f
YuFeng Deng <yufeng.deng@catchsoftware.com>
2019-11-23 10:29:33 +1300

    try catch in then

--- myapp/routes/telegram.js
+++ myapp/routes/telegram.js
@@ -90,7 +90,9 @@ function(req, res, next) {
     }
   }).then(x => {
     console.log("这段代码在200后面, 而且只能放在then外面");
-    console.log(x.bbb.ccc);
+    try {
+      console.log(x.bbb.ccc);
+    } catch (e) {}
   });
 
   console.log('做点别的什么事, 不需要在200后面');
